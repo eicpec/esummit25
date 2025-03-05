1915d541399998c4858b95a68d3d85fe8884e848
Karrtik Gupta <karrtikschoolid11@gmail.com>
2025-03-05 23:29:19 +0530

    Latest files

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react';
 import { ExpandableCardDemo } from './pages/events/competitions.jsx';
 import Team from './pages/team.jsx';
+import Header from './layouts/Header.jsx';
+import Footer from './layouts/Footer.jsx';
 
 function App() {
   return (
     <div className= "App" >
+      <Header />
       <ExpandableCardDemo />
       <Team />
+      <Footer />
     </div>
   );
 }
