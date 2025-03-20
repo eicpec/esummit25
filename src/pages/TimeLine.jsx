43c3c0c394ff8dc620e5000707add9f4e2a76e75
Karrtik Gupta <karrtikschoolid11@gmail.com>
2025-03-20 09:29:44 +0530

    IPL Auction

--- src/pages/TimeLine.jsx
+++ src/pages/TimeLine.jsx
@@ -4,17 +4,18 @@ import DAY1 from "../assets/Schedule/1.png";
 import DAY2 from "../assets/Schedule/2.png";
 import Soon from "../../public/soon.png"
 import Guess from "../assets/Dignitaries/guess.png"
-
+import sch1 from "../team/sch1.png";
+import sch2 from "../team/sch2.png";
 function TimelineDemo() {
     return (
         <Layout>
             <div className="flex flex-col md:flex-row mt-20 items-center gap-5 justify-center">
-                {/* <div className="my-6 w-full md:w-1/2 h-screen bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${Guess})` }}></div>
-                <div className="my-6 w-full md:w-1/2 h-screen bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${Guess})` }}></div> */}
+                <div className="my-6 w-full md:w-1/2 h-screen bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${sch1})` }}></div>
+                <div className="my-6 w-full md:w-1/2 h-screen bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${sch2})` }}></div>
                 {/* <div className="my-6 w-full md:w-1/2 h-screen bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${Soon})` }}></div> */}
-                <div className="flex h-screen w-full items-center justify-center">
+                {/* <div className="flex h-screen w-full items-center justify-center">
                     <h1 className="text-5xl text-white self-center text-center">Releasing Soon</h1>
-                </div>
+                </div> */}
             </div>
         </Layout>
     );
